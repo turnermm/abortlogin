19f2c418381d2e4dbad44f8a237a23921e6e9cac
Myron Turner <turnermm02@shaw.ca>
2015-08-06 16:43:29 -0500

    correct typo

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,4 +1,4 @@
 <?php
 $lang['allowed'] = "Comma separated list of allowed ip addresses";
-$lang['test'] = "Comma separated list of ip addresses to test; testing limited admins only";
+$lang['test'] = "Comma separated list of ip addresses to test; testing limited to admins only";
 $lang['log'] ="Keep a log of failed login attempts";
