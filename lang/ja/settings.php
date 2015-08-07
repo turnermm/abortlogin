3ed385362cfd64e71d4f5c8fbf72912b2d094e14
Hideaki SAWADA <chuno@live.jp>
2015-08-07 20:37:21 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['allowed']        = "許可する IP アドレスのカンマ区切り一覧";
+$lang['test']           = "テスト用の IP アドレスのカンマ区切り一覧。admin グループに対してだけテストします。";
+$lang['log']            = "失敗したログイン試行のログを保存する";
