d2fda57ebf378ab93b09f0f02df9c3af52c88dcf
Myron Turner <turnermm02@shaw.ca>
2017-12-08 11:35:27 -0600

    updating for ipv6

--- action.php
+++ action.php
@@ -43,7 +43,7 @@ class action_plugin_abortlogin extends DokuWiki_Action_Plugin
              unset($USERINFO) ;
              global $ACT;  $ACT = 'logout';          
       }   
-      
+    
       if($test && isset($USERINFO) && in_array('admin', $USERINFO['grps'])) {         
           $tests = explode(',',$test);
           foreach ($tests as $test) {           
@@ -67,6 +67,35 @@ class action_plugin_abortlogin extends DokuWiki_Action_Plugin
     } 
     
      function is_allowed($allowed, $ip) {
+         if ($this->valid_ipv6_address( $ip )){
+             //msg("valid ip6 format: $ip");
+             return ($this->is_allowed_v6($allowed, $ip));
+         }
+        
+         
+      if( preg_match('/(\.\d+)+/',$ip)) 
+     {
+          //msg("valid ip4 format: $ip");
+         return ($this->is_allowed_v4($allowed, $ip));
+     } 
+        return false;
+    }   
+
+    function is_allowed_v6($allowed, $ip) {
+        $orig = $ip;
+          $allowed =  trim($allowed,', '); 
+          $ip = $this->ipaddress_to_ipnumber($ip); 
+          $ar = explode(",",$allowed);
+          
+          foreach ($ar as $addr) {
+                $test = $this->ipaddress_to_ipnumber($addr);
+             //   msg("testing: ($orig) $ip  against $addr: " . $test);
+                if($ip == $test) return true;  
+            } 
+          return false;
+     }   
+     
+    function is_allowed_v4($allowed, $ip) {
          static $cache = '';     
          
          if($cache) {
@@ -85,6 +114,25 @@ class action_plugin_abortlogin extends DokuWiki_Action_Plugin
         return false;  
      }
      
+    function valid_ipv6_address( $ipv6 )
+    {
+        $regex = '/^(((?=(?>.*?(::))(?!.+\3)))\3?|([\dA-F]{1,4}(\3|:(?!$)|$)|\2))(?4){5}((?4){2}|(25[0-5]|(2[0-4]|1\d|[1-9])?\d)(\.(?7)){3})\z/i';
+            if(!preg_match($regex, $ipv6))
+            return (false); // is not a valid IPv6 Address
+
+        return true;
+    }
+
+    function ipaddress_to_ipnumber($ipaddress) {
+        $pton = @inet_pton($ipaddress);
+        if (!$pton) { return false; }
+        $number = '';
+        foreach (unpack('C*', $pton) as $byte) {
+            $number .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
+        }
+        return base_convert(ltrim($number, '0'), 2, 10);
+    }
+
      function log($ip) {         
         $log = metaFN('abortlogin:aborted_ip','.log');   
         io_saveFile($log,"$ip\n",1);
